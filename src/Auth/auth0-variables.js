9961ac11072cfe77b336ae5b6ce3aa4dfbee41a3
JeffAll <jiffall@gmail.com>
2018-10-14 17:49:41 -0500

    dump

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'jefall.auth0.com',
+  clientId: 'cJvGJ3Mpan5HRFFGBBtfg6ch4E2cu10f',
+  callbackUrl: 'http://localhost:3000/callback',
+  audience: 'https://jeffall.com/magi'
+}
